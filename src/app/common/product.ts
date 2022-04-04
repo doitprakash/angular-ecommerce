d2f02e96a3e6fb17dc3140a996dc2a2f3b4352b4
Prakash <prakashranjantripathi@gmail.com>
2022-04-04 11:37:39 +0530

    front end uploaded

---
+++ src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    sku: string;
+    name: string;
+    description: string;
+    unitprice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
+}
